c46b3b038dc875541a87ed6c097dd3d80a81890d
bernardothives <bernardoalvesthives@gmail.com>
2025-04-16 12:16:19 -0300

    add passwords

--- src/app/secretarias/secretariasData.ts
+++ src/app/secretarias/secretariasData.ts
@@ -1,133 +1,133 @@
 export const secretariasContent = {
     'secretaria-de-administracao': {
       title: 'Secretaria de Administração',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19z05PXicC7mD+FVJsL+PaS27zCRlC5Dwc=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=adm-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EZMC6q59GSpJjppq7YluMrkBDlEA4vgOeLyv7z53e91EMw?e=lW2q90',
       content: ''
     },
     'secretaria-de-assistencia-social': {
       title: 'Secretaria de Assistência Social',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/3sKjuWxUpi+TlRRvmLlhnxyOn+u8XS+U=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=assistencia-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EeYFpEyjyXhPgS_EwEM1jVkBucIOGbkRgqRcwycKy4hdjA?e=bdqDNn',
       content: ''
     },
     'secretaria-de-cultura-e-turismo': {
       title: 'Secretaria de Cultura e Turismo',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19QYfUVAfJq78jr1aMitnl6lARWXyU1ZD0=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=cultura-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EYegAbsbxt5FmY11kGhSQrQB28wGrP_d5mS0TfyKJyVvQw?e=ZhYrtz',
       content: ''
     },
     'secretaria-integrada-de-comunicacao': {
       title: 'Secretaria Integrada de Comunicação',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX18AHpUqDHYeeZpwuddKqyEwpaZzLWAN3vs=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=comunicacao-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/ES30mYvzXxRGkScCEDJnW6MBH9d1yd5U8V5XVqfiFdGImw?e=UUCfhl',
       content: ''
     },
     'secretaria-de-desenvolvimento-economico-e-inovacao': {
       title: 'Secretaria de Desenvolvimento Econômico e Inovação',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX18m6SLIVGGQ+G4sYgV56PueV9XnCKWHpf8=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=desenvolvimento-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EQglDNf6_0NEtSCpeQXXfmkBp3UadI-g7Rw2Qz-_ihLpZQ?e=X9nK7J',
       content: ''
     },
     'secretaria-de-educacao': {
       title: 'Secretaria de Educação',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19L6BLpx4Mr265nCLPdDZCAeQw2YBDJxNs=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=educacao-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EeKLllDHbCVPt8yM3sGqtfIBTVZbULX12ruRNcvLKixD5g?e=BjxfZb',
       content: ''
     },
     'secretaria-de-esportes-e-lazer': {
       title: 'Secretaria de Esportes e Lazer',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19Atj7lhRDze/qIWejll8qn2yaZU6R+Qbg=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=esportes-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EZa_tqa2chtGgg91ZUB1udkBrQCjGsT0EKkFhMjEH4P8eA?e=dXqXlG',
       content: ''
     },
     'secretaria-de-financas': {
       title: 'Secretaria de Finanças',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/Fvf5Yr4fFlfRgw37U5grUFk6Sse/crUA=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=financas-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/ESJ032ewTOVCqfG478kpm8IBIEPJ_9MbuNEC3JNj6a9YXw?e=s2JeLN',
       content: ''
     },
     'secretaria-de-governo': {
       title: 'Secretaria de Governo',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/IxFGkmKNI8/Rx2xqgJrhB+ADo+Ho2Bw4=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=governo-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EdI1uZwl2fVAg09mCf--VqgBTNLjGQ7cCDQo8dbPnGydvg?e=PeoGi1',
       content: ''
     },
     'secretaria-de-infraestrutura': {
       title: 'Secretaria de Infraestrutura',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19qR1ihHwwX5tfHDIuEbJ8YgKdpPQK2d/Y=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=infraestrutura-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EfywN6sHavpIumux9La3i_sBqD53sieBpvM9UzcPQM17YA?e=8UciZH',
       content: ''
     },
     'secretaria-de-planejamento-e-assuntos-estrategicos': {
       title: 'Secretaria de Planejamento e Assuntos Estratégicos',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1+ZErWRnxF+4ImSE/7aOdayP261bYPkpu0=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=planejamento-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/Earq9embX_pBvu00r5cHeZ8Bw_DCkf3KixPMfK1eJUu1nQ?e=L6bY7O',
       content: ''
     },
     'secretaria-da-receita': {
       title: 'Secretaria da Receita',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX18IXKZEKTNHtcnNde/csJ5YHo6H9fz25dg=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=receita-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/Ea4eLn4G7LxAvlzIOPALvIUB9qnihLnmbYRWQiGnnTf9zg?e=VgUCpx',
       content: ''
     },
     'secretaria-de-regularizacao-fundiaria': {
       title: 'Secretaria de Regularização Fundiária',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/dIrdxpEJF+B0LJ29/NeXNds5Fg0v1Q2s=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=regularizacao-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/ERKLkc80kCVLuS1nyBiLM3EBGrQmoeZt1HG4wmpUGa3gww?e=hj5czn',
       content: ''
     },
     'secretaria-de-saude': {
       title: 'Secretaria de Saúde',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/qEL9DG/TkbdOe3O8OYbJCfvAsGKFNMSg=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=saude-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/ESb3ziamf-5MsyU85jZ0WKYBj-xtHDnd-FBflyKG66RfzA?e=hbAVkZ',
       content: ''
     },
     'secretaria-de-seguranca-defesa-social-e-transito': {
       title: 'Secretaria de Segurança, Defesa Social e Trânsito',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX19sNplePq86qj5kJHAEm9/Wh/A5DoYJlMA=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=seguranca-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EdbKKoFMKMpIjy_pz8MKLekB4zas6_0dzKfPJ50FzrZ7hQ?e=VSzE2h',
       content: ''
     },
     'secretaria-de-transparencia': {
       title: 'Secretaria de Transparência',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/1ZO7lR7z+1Pxdjz0bZPBWjJrvfZFj04A=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=transparencia-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EY12StKGtJBMpEpidBEFxT0B8d9CxaLeGkvqLuTVb79vAg?e=Spy6Ea',
       content: ''
     },
     'secretaria-de-urbanismo-e-servicos-publicos': {
       title: 'Secretaria de Urbanismo e Serviços Públicos',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX1/+v0pB1GqTxG8fapS6sIE0OtdtMXOww1I=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=urbanismo-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/Ea_3nS1wczJGgS2PqQkZieIBsF2z2FLenjuBVIsutEL0fQ?e=w59ofb',
       content: ''
     },
     'fundacao-educacional-municipal': {
       title: 'Fundação Educacional Municipal',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX188hm8hmoX+4Bc16DAOxsbxOrm6CyBrhWE=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=educacional-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EUUdpsZ3fudDoZwsbnio5yABW1H2ShXDI1bnamrLu7F0Ug?e=EZ48lE',
       content: ''
     },
     'fundacao-municipal-do-meio-ambiente-e-desenvolvimento-sustentavel': {
       title: 'Fundação Municipal do Meio Ambiente e Desenvolvimento Sustentável',
-      encryptedContent: 'U2FsdGVkX1/VWS6xeGe4WQV5OGgLg6Eqh3PCAd3adxk=',
+      encryptedContent: 'U2FsdGVkX192+wz/erppnYYlGKFnXwcLKagBGegbA+c=',
       powerBIEmbedUrl: 'https://app.powerbi.com/view?r=meioambiente-placeholder',
       sheetUrl: 'https://1drv.ms/x/c/e7d9cc0b8185d601/EX4owBHYnudEsX2jUjc_aTsBnqcqGVnm_rnLp4INfhvICQ?e=rxOQKH',
       content: ''
